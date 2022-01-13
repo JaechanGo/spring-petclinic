d978734594b879731bee2116328af22cdd00cbcf
jaechango_cudo <happypuki@gmail.com>
2022-01-13 10:25:50 +0900

    2022.01.13_s08

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -20,6 +20,7 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -44,9 +45,9 @@ import org.springframework.web.servlet.ModelAndView;
 class OwnerController {
 
 	private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
-
 	private final OwnerRepository owners;
 
+	@Autowired
 	public OwnerController(OwnerRepository clinicService) {
 		this.owners = clinicService;
 	}
